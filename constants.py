dfc5c59b0e859b41ddf7263a8d2b0b62dc6a8070
Jayesh Koshi <jayeshkoshi@gmail.com>
2025-02-26 16:27:39 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-API_KEY = "be4c6e736af7aed001fd9407e36f59ed"  # Replace with your TMDb API key
+API_KEY = ""  # Replace with your TMDb API key
 BASE_URL = "https://api.themoviedb.org/3"
 WATCHLIST_FILE = "watchlist.json"
 WATCHED_FILE = "watched.json"
